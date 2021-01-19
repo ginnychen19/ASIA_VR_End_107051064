0b672b69ca7ab3d73da678b03dde04a42746fa73
ginnychen19 <71680206+ginnychen19@users.noreply.github.com>
2021-01-19 12:24:55 +0800

    完成發布黨

---
+++ VR_End_107051064/Assets/script/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class GameManager : MonoBehaviour
+{
+    
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("S1");
+    }
+
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void QuitGame()
+    {
+       
+        Application.Quit();
+    }
+}
